7b5ddfaab15bca859ceebfd4ebb1695af4a624b1
tatwd <tatwdo@gmail.com>
2023-03-11 22:24:37 +0800

    feat: add ToUtcString and update comments

--- FeedSharp/Internals/Utils.cs
+++ FeedSharp/Internals/Utils.cs
@@ -4,17 +4,31 @@ namespace FeedSharp.Internals;
 
 internal static class Utils
 {
+    /// <summary>
+    /// Default generator name
+    /// </summary>
     public static string Generator =>  "https://github.com/tatwd/FeedSharp";
     
-    public static string? Sanitize(string? url)
-    {
-        if (url is null)
-        {
-            return url;
-        }
-        return url.Replace("&", "&amp;");
-    }
+    /// <summary>
+    /// Check and replace '&amp;' to '&amp;amp;'
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public static string? Sanitize(string? url) => url is null ? url : url.Replace("&", "&amp;");
 
+    /// <summary>
+    /// Format DateTime just like '2023-03-11T14:19:32.673Z'
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns></returns>
     public static string ToIsoString(DateTime dateTime) =>
         dateTime.ToUniversalTime().ToString("yyyy-MM-dd'T'HH:mm:ss.fffK", CultureInfo.InvariantCulture);
+    
+    /// <summary>
+    /// Format DateTime just like 'Wed, 01 Nov 2023 12:11:00 GMT'
+    /// </summary>
+    /// <param name="dateTime"></param>
+    /// <returns></returns>
+    public static string ToUtcString(DateTime dateTime) =>
+        dateTime.ToUniversalTime().ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'", CultureInfo.InvariantCulture);
 }
